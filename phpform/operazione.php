4599e6072a46a59f5026f3547725abcfc2893802
amm <amm@amm-VirtualBox>
2015-04-15 18:20:14 +0200

    provaphp numero 5

--- phpform/operazione.php
+++ phpform/operazione.php
@@ -3,20 +3,29 @@ $primo = $_POST["primo"];
 $secondo = $_POST["secondo"];
 $operando = $_POST["operation"];
 
-switch ($operando){
-    case "+":
-        $risultato = $primo+$secondo;
-        break;
-    case "-":
-        $risultato = $primo-$secondo;
-        break;
-    case "x":
-        $risultato = $primo*$secondo;
-        break;
-    case "/":
-        $risultato = $primo/$secondo;
-        break;
+$val1= filter_var($primo, FILTER_VALIDATE_FLOAT, FILTER_NULL_ON_FAILURE);
+$val2= filter_var($secondo, FILTER_VALIDATE_FLOAT, FILTER_NULL_ON_FAILURE);
+
+if($val1&&$val2){
+    echo("entra");
+    switch ($operando){
+        case "+":
+            $risultato = $primo+$secondo;
+            break;
+        case "-":
+            $risultato = $primo-$secondo;
+            break;
+        case "x":
+            $risultato = $primo*$secondo;
+            break;
+        case "/":
+            $risultato = $primo/$secondo;
+            break;
+    }
+    echo "$primo $operando $secondo = $risultato ";
 }
-echo "$primo $operando $secondo = $risultato ";
+else{
+    echo "Qualcosa non va";
+}
 
 ?>

ee92bda459ce35eb0dec03fd97fab34b11c94c6a
Luca Pitzalis <lucapizza@gmail.com>
2015-07-09 12:02:09 +0200

    Controller

--- Progetto/index.php
+++ Progetto/index.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <h1>Hello there</h1>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+    include_once dirname(__FILE__) . '/php/Controller/NotLoggedController.php';    
+    
+    $_REQUEST["cmd"]="NotLogged";
+    $NotLoggedController = new NotLoggedController();
+    $NotLoggedController->invoke($_REQUEST);
+    
+ 
+?>
